d2205e542111e56fec3a68d04c6eef88a99ee40f
SabarishCodeWizard <ravik60656@gmail.com>
2024-09-18 17:34:33 +0530

    added a github green color

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="SabarishCodeWizard"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#28a745"
         fontSize={16}
       />
     </Row>
